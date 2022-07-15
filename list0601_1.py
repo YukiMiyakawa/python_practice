a5b4d4ffba90b91b96173b165e5115183bf0ba26
YukiMiyakawa <sakomiya3838@gmail.com>
2022-07-15 22:45:31 +0900

    GUI pravtice strat

---
+++ list0601_1.py
@@ -0,0 +1,11 @@
+import tkinter
+import tkinter.font
+root = tkinter.Tk()
+# root.title("初めてのウィンドウ")
+# root.title("初めてのラベル")
+# root.geometry("800x600")
+# label = tkinter.Label(root, text="ラベルの文字列", font=("System", 24))
+# label.place(x=200, y=100)
+# root.mainloop()
+
+print(tkinter.font.families())
